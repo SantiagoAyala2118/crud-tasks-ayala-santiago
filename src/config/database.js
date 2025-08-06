c7caec49f7403beefb62fd8a56fbdcd5fb35b8e1
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-06 15:23:35 -0300

    Se completaron los archivos database y db en el directorio config

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+import { Task } from "../models/tasks.model.js";
+import { User } from "../models/users.model.js";
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
