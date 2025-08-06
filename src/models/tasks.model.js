f6cfcc99d4719baf1a6ab225b88606d5a6215323
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-06 15:57:46 -0300

    Se completaron los modelos en el directorio models

---
+++ src/models/tasks.model.js
@@ -0,0 +1,17 @@
+import { sequelize } from "../config/database.js";
+import { DataTypes } from "sequelize";
+export const Task = sequelize.define("Task", {
+  title: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING(100),
+    unique: true,
+    allowNull: false,
+  },
+  isComplete: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
