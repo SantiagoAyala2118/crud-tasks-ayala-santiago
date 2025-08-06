beebe79954f99937ae5388a7991759def2234700
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-06 16:47:59 -0300

    se avanzó un poco en los controladores del task en el directorio controller

---
+++ src/controller/tasks.controller.js
@@ -0,0 +1,8 @@
+import { Task } from "../models/tasks.model.js";
+
+export const createTask = async () => {
+  try {
+  } catch (err) {
+    console.error("An error has happened while creating a task", err);
+  }
+};
