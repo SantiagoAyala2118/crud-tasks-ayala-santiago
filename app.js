94ec4389e97fc8a948a5dcafdc114eb0b5281e4f
Santiago Ayala <santitomayala07@gmail.com>
2025-08-06 21:17:29 -0300

    Se terminó la estructura del app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import userRouter from './src/routes/user.routes.js';
+import taskRouter from './src/routes/task.routes.js';
+import dotenv from 'dotenv';
+import { initDB } from './src/config/db.js';
+dotenv.config();
+
+initDB();
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.DB_PORT;
+
+app.use('/', userRouter);
+app.use('/', taskRouter);
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`)
+})
+
