5b9dec61e0a6b6b74f207f00b78c903564ecb1a9
Santiago Ayala <santitomayala07@gmail.com>
2025-08-13 20:44:32 -0300

    Comentarios para guía agregados

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,8 +10,8 @@ import {
 const userRouter = Router();
 
 userRouter.post("/api/users", createUser);
-userRouter.get("/api/users", getAllUsers);
-userRouter.get("/api/users/:id", getUser);
+userRouter.get("/api/users", getAllUsers); //trae todos los usuarios con sus tareas
+userRouter.get("/api/users/:id", getUser); //trae un usuario con sus tareas correspondientes
 userRouter.put("/api/users/:id", updateUser);
 userRouter.delete("/api/users/:id", deleteUser);
 
