9c0e478bfcf9190c8bb433f9fe75b8ea0bdec807
Santiago Ayala <santitomayala07@gmail.com>
2025-08-15 11:11:20 -0300

    createUserProject funcionando y validaciones añadidas

---
+++ src/controller/users_projects.controllers.js
@@ -0,0 +1,51 @@
+import { User_Project } from "../models/users_projects.model.js";
+import { User } from "../models/users.model.js";
+import { Project } from "../models/projects.model.js";
+
+export const createUserProject = async (req, res) => {
+    try {
+        const { user_id, project_id } = req.body;
+
+        if (!user_id || !project_id) {
+            return res.status(400).json({
+                message: 'The user_id and project_id are neccessary'
+            });
+        };
+
+        if (typeof user_id !== 'number' || typeof project_id !== 'number') {
+            return res.status(400).json({
+                message: 'The user_id and project_id must be a number'
+            });
+        };
+
+        const userIdExistig = await User.findByPk(user_id)
+        if (!userIdExistig) {
+            return res.status(400).json({
+                message: 'There are no users with that id in the database'
+            });
+        };
+
+        const projectIdExisting = await Project.findByPk(project_id)
+        if (!projectIdExisting) {
+            return res.status(400).json({
+                message: 'There are no projects with that id in the database'
+            });
+        };
+
+
+
+        const userProject = await User_Project.create({
+            user_id,
+            project_id
+        });
+
+        if (userProject) {
+            return res.status(201).json({
+                userProject
+            });
+        };
+
+    } catch (err) {
+        console.error('An error has occurred while creating an user project', err)
+    };
+};
