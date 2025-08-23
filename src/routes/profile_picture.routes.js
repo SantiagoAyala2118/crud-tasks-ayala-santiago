63bdc40cb967e5b5b289d34e981e99f8408ddf6a
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-23 12:37:53 -0300

    Rutas de delete y update agregadas correctamente

--- src/routes/profile_picture.routes.js
+++ src/routes/profile_picture.routes.js
@@ -2,11 +2,15 @@ import { Router } from "express";
 import {
   createProfilePicture,
   getAllProfilePictures,
+  updateProfilePicture,
+  deleteProfilePicture,
 } from "../controller/profile_picture.controller.js";
 
 const profilePictureRouter = Router();
 
 profilePictureRouter.post("/api/profile_picture", createProfilePicture);
 profilePictureRouter.get("/api/profile_picture", getAllProfilePictures);
+profilePictureRouter.put("/api/profile_picture/:id", updateProfilePicture);
+profilePictureRouter.delete("/api/profile_picture/:id", deleteProfilePicture);
 
 export default profilePictureRouter;
