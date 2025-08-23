eed53e12cb82c672f95ee2b2a0700a35728f3963
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-23 20:14:20 -0300

    Rutas de delete update y get one agregadas correctamente

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { applyValidations } from "../middlewares/validator.js";
 import { createUserValidations } from "../middlewares/validations/userValidations/createUserValidations.js";
+import { getOneUserValidations } from "../middlewares/validations/userValidations/getOneUserValidations.js";
+import { updateUserValidations } from "../middlewares/validations/userValidations/updateUserValidations.js";
 import {
   createUser,
   getAllUsers,
@@ -8,6 +10,7 @@ import {
   updateUser,
   deleteUser,
 } from "../controller/user.controller.js";
+import { deleteUserValidation } from "../middlewares/validations/userValidations/deleteUserValidations.js";
 
 const userRouter = Router();
 
@@ -18,8 +21,23 @@ userRouter.post(
   createUser
 );
 userRouter.get("/api/users", getAllUsers); //trae todos los usuarios con sus tareas
-userRouter.get("/api/users/:id", getUser); //trae un usuario con sus tareas correspondientes
-userRouter.put("/api/users/:id", updateUser);
-userRouter.delete("/api/users/:id", deleteUser);
+userRouter.get(
+  "/api/users/:id",
+  getOneUserValidations,
+  applyValidations,
+  getUser
+); //trae un usuario con sus tareas correspondientes
+userRouter.put(
+  "/api/users/:id",
+  updateUserValidations,
+  applyValidations,
+  updateUser
+);
+userRouter.delete(
+  "/api/users/:id",
+  deleteUserValidation,
+  applyValidations,
+  deleteUser
+);
 
 export default userRouter;
