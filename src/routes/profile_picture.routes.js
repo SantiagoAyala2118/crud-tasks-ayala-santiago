d9477693050bb1949bba9b2dc8196e45a05e836b
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-23 12:48:50 -0300

    Ruta de get one pfp agregada correctamente

--- src/routes/profile_picture.routes.js
+++ src/routes/profile_picture.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   createProfilePicture,
   getAllProfilePictures,
+  getOneProfilePicture,
   updateProfilePicture,
   deleteProfilePicture,
 } from "../controller/profile_picture.controller.js";
@@ -10,6 +11,7 @@ const profilePictureRouter = Router();
 
 profilePictureRouter.post("/api/profile_picture", createProfilePicture);
 profilePictureRouter.get("/api/profile_picture", getAllProfilePictures);
+profilePictureRouter.get("/api/profile_picture/:id", getOneProfilePicture);
 profilePictureRouter.put("/api/profile_picture/:id", updateProfilePicture);
 profilePictureRouter.delete("/api/profile_picture/:id", deleteProfilePicture);
 
