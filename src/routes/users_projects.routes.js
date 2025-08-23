189618fd717c4fd214b4e34a2b77e4c3b72319cf
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-23 14:09:48 -0300

    Rutas get one, update y delete agregadas correctamente

--- src/routes/users_projects.routes.js
+++ src/routes/users_projects.routes.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import {
-    createUserProject,
-    getAllUsersProjects
-} from '../controller/users_projects.controllers.js';
+  createUserProject,
+  getAllUsersProjects,
+  getOneUserProject,
+  updateUserProject,
+  deleteUserProject,
+} from "../controller/users_projects.controllers.js";
 
 const usersProjectsRouter = Router();
 
-usersProjectsRouter.post('/api/users_projects', createUserProject);
-usersProjectsRouter.get('/api/users_projects', getAllUsersProjects);
+usersProjectsRouter.post("/api/users_projects", createUserProject);
+usersProjectsRouter.get("/api/users_projects", getAllUsersProjects);
+usersProjectsRouter.get("/api/users_projects/:id", getOneUserProject);
+usersProjectsRouter.put("/api/users_projects/:id", updateUserProject);
+usersProjectsRouter.delete("/api/users_projects/:id", deleteUserProject);
 
 export default usersProjectsRouter;
