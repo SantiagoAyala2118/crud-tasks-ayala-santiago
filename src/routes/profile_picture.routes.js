af3e884c4e77fcfba2d429e6eff5c7ce259d19fd
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-24 12:57:34 -0300

    Rutas delete, update y get one refactorizadas correctamente

--- src/routes/profile_picture.routes.js
+++ src/routes/profile_picture.routes.js
@@ -1,4 +1,8 @@
 import { Router } from "express";
+import { createProfilePictureValidations } from "../middlewares/validations/profilePictureValidations/createProfilePictureValidations.js";
+import { getOneProfilePictureValidations } from "../middlewares/validations/profilePictureValidations/getOneProfilePictureValidations.js";
+import { updateProfilePictureValidations } from "../middlewares/validations/profilePictureValidations/updateProfilePictureValidations.js";
+import { deleteProfilePictureValidations } from "../middlewares/validations/profilePictureValidations/deleteProfilePictureValidations.js";
 import {
   createProfilePicture,
   getAllProfilePictures,
@@ -6,13 +10,34 @@ import {
   updateProfilePicture,
   deleteProfilePicture,
 } from "../controller/profile_picture.controller.js";
+import { applyValidations } from "../middlewares/validator.js";
 
 const profilePictureRouter = Router();
 
-profilePictureRouter.post("/api/profile_picture", createProfilePicture);
+profilePictureRouter.post(
+  "/api/profile_picture",
+  createProfilePictureValidations,
+  applyValidations,
+  createProfilePicture
+);
 profilePictureRouter.get("/api/profile_picture", getAllProfilePictures);
-profilePictureRouter.get("/api/profile_picture/:id", getOneProfilePicture);
-profilePictureRouter.put("/api/profile_picture/:id", updateProfilePicture);
-profilePictureRouter.delete("/api/profile_picture/:id", deleteProfilePicture);
+profilePictureRouter.get(
+  "/api/profile_picture/:id",
+  getOneProfilePictureValidations,
+  applyValidations,
+  getOneProfilePicture
+);
+profilePictureRouter.put(
+  "/api/profile_picture/:id",
+  updateProfilePictureValidations,
+  applyValidations,
+  updateProfilePicture
+);
+profilePictureRouter.delete(
+  "/api/profile_picture/:id",
+  deleteProfilePictureValidations,
+  applyValidations,
+  deleteProfilePicture
+);
 
 export default profilePictureRouter;
