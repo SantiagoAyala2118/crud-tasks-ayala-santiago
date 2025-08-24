dd327703a0c69a04e308965a2775acffa2690bb6
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-24 15:48:47 -0300

    Rutas update, delete y get one refactorizadas correctamente

--- src/routes/users_projects.routes.js
+++ src/routes/users_projects.routes.js
@@ -1,4 +1,8 @@
 import { Router } from "express";
+import { createUserProjectValidations } from "../middlewares/validations/userProjectValidations/createUserProjectValidations.js";
+import { getOneUserProjectValidations } from "../middlewares/validations/userProjectValidations/getOneUserProjectValidations.js";
+import { updateUserProjectValidations } from "../middlewares/validations/userProjectValidations/updateUserProjectValidations.js";
+import { deleteUserProjectValidations } from "../middlewares/validations/userProjectValidations/deleteUserProjectValidations.js";
 import {
   createUserProject,
   getAllUsersProjects,
@@ -6,13 +10,34 @@ import {
   updateUserProject,
   deleteUserProject,
 } from "../controller/users_projects.controllers.js";
+import { applyValidations } from "../middlewares/validator.js";
 
 const usersProjectsRouter = Router();
 
-usersProjectsRouter.post("/api/users_projects", createUserProject);
+usersProjectsRouter.post(
+  "/api/users_projects",
+  createUserProjectValidations,
+  applyValidations,
+  createUserProject
+);
 usersProjectsRouter.get("/api/users_projects", getAllUsersProjects);
-usersProjectsRouter.get("/api/users_projects/:id", getOneUserProject);
-usersProjectsRouter.put("/api/users_projects/:id", updateUserProject);
-usersProjectsRouter.delete("/api/users_projects/:id", deleteUserProject);
+usersProjectsRouter.get(
+  "/api/users_projects/:id",
+  getOneUserProjectValidations,
+  applyValidations,
+  getOneUserProject
+);
+usersProjectsRouter.put(
+  "/api/users_projects/:id",
+  updateUserProjectValidations,
+  applyValidations,
+  updateUserProject
+);
+usersProjectsRouter.delete(
+  "/api/users_projects/:id",
+  deleteUserProjectValidations,
+  applyValidations,
+  deleteUserProject
+);
 
 export default usersProjectsRouter;
