b1749f6eda80a3cf1e4e9ec0a06e04e6fad93dd7
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-24 16:45:57 -0300

    Reestructuración del validador para que muestre de mejor manera los errores

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -3,8 +3,11 @@ import { validationResult } from "express-validator";
 export const applyValidations = (req, res, next) => {
   const errors = validationResult(req);
 
+  const custom = errors.formatWith((err) => {
+    return `${err.param}: ${err.msg}`;
+  });
   if (!errors.isEmpty()) {
-    return res.status(400).json(errors.mapped());
+    return res.status(400).json({ errors: custom.mapped() });
   }
 
   next();
