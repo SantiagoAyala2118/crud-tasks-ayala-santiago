73d3d5f153da327095fb9f5b3c8374078fbc8c10
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-23 22:01:24 -0300

    Rutas update, get one y delete refactorizadas correctamente

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,4 +1,8 @@
 import { Router } from "express";
+import { createTaskValidations } from "../middlewares/validations/taskValidations/createTaskValidations.js";
+import { getOneTaskValidation } from "../middlewares/validations/taskValidations/getOneTaskValidation.js";
+import { updateTaskValidations } from "../middlewares/validations/taskValidations/updateTaskValidation.js";
+
 import {
   createTask,
   getAllTasks,
@@ -6,14 +10,35 @@ import {
   updateTask,
   deleteTask,
 } from "../controller/tasks.controller.js";
+import { applyValidations } from "../middlewares/validator.js";
+import { deleteTaskValidations } from "../middlewares/validations/taskValidations/deleteTaskValidations.js";
 
 const taskRouter = Router();
 
-taskRouter.post("/api/tasks", createTask); //crear una tarea relacionada a un usuario
+taskRouter.post(
+  "/api/tasks",
+  createTaskValidations,
+  applyValidations,
+  createTask
+); //crear una tarea relacionada a un usuario
 taskRouter.get("/api/tasks", getAllTasks); //trae todas las tareas con el usuario que las creó
-taskRouter.get("/api/tasks/:id", getTask); //trae una tarea específica junto al usuario relacionado
-taskRouter.put("/api/tasks/:id", updateTask);
-taskRouter.delete("/api/tasks/:id", deleteTask);
-
+taskRouter.get(
+  "/api/tasks/:id",
+  getOneTaskValidation,
+  applyValidations,
+  getTask
+); //trae una tarea específica junto al usuario relacionado
+taskRouter.put(
+  "/api/tasks/:id",
+  updateTaskValidations,
+  applyValidations,
+  updateTask
+);
+taskRouter.delete(
+  "/api/tasks/:id",
+  deleteTaskValidations,
+  applyValidations,
+  deleteTask
+);
 
 export default taskRouter;
