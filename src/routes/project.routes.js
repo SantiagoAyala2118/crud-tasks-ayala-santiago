f8b9d03c967e70421875ab0d15870a665753c9fb
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-24 09:35:20 -0300

    Routas update, delete y get one refactorizadas correctamente

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,4 +1,8 @@
 import { Router } from "express";
+import { createProjectValidations } from "../middlewares/validations/projectValidations/createProjectValidations.js";
+import { getOneProjectValidations } from "../middlewares/validations/projectValidations/getOneProjectValidations.js";
+import { updateProjectValidations } from "../middlewares/validations/projectValidations/updateProjectValidations.js";
+import { deleteProjectValidations } from "../middlewares/validations/projectValidations/deleteProjectValidations.js";
 import {
   createProject,
   getAllProjects,
@@ -6,13 +10,34 @@ import {
   updateProject,
   deleteProject,
 } from "../controller/project.controller.js";
+import { applyValidations } from "../middlewares/validator.js";
 
 const projectRouter = Router();
 
-projectRouter.post("/api/project", createProject);
+projectRouter.post(
+  "/api/project",
+  createProjectValidations,
+  applyValidations,
+  createProject
+);
 projectRouter.get("/api/project", getAllProjects);
-projectRouter.get("/api/project/:id", getOneProject);
-projectRouter.put("/api/project/:id", updateProject);
-projectRouter.delete("/api/project/:id", deleteProject);
+projectRouter.get(
+  "/api/project/:id",
+  getOneProjectValidations,
+  applyValidations,
+  getOneProject
+);
+projectRouter.put(
+  "/api/project/:id",
+  updateProjectValidations,
+  applyValidations,
+  updateProject
+);
+projectRouter.delete(
+  "/api/project/:id",
+  deleteProjectValidations,
+  applyValidations,
+  deleteProject
+);
 
 export default projectRouter;
